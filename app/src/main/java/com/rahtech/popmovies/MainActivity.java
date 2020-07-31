0cb5b800e661201e315b52151186ac041460a51f
RajasekharGuptha <rajasekharmuppidi4@gmail.com>
2020-07-31 15:29:07 +0530

    Included search function model

--- app/src/main/java/com/rahtech/popmovies/MainActivity.java
+++ app/src/main/java/com/rahtech/popmovies/MainActivity.java
@@ -1,13 +1,16 @@
 package com.rahtech.popmovies;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -79,6 +82,45 @@ public class MainActivity extends AppCompatActivity implements adapter.listItemC
         upcomingRecyclerView.setAdapter(upcomingadapter);
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        // Assumes current activity is the searchable activity
+        if (searchManager != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(new MainActivity().getComponentName()));
+        }
+        searchView.setIconifiedByDefault(true);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                String searchBaseUrl="https://api.themoviedb.org/3/search/movie?api_key=<api_key>&query=";
+
+                //if # in search query condiser string after # as year for simplicity
+//                if (query.contains("#")){
+//                     new getData().getDataFromUrl(searchBaseUrl+query+"?year="+query.split("#")[1],"mention_type_here");
+//
+//                }
+//                //use this data
+                // new getData().getDataFromUrl(searchBaseUrl+query,"mention_type_here");
+
+
+                return true;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+
+            }
+        });
+        return true;
+    }
+
 
 
     @Override
